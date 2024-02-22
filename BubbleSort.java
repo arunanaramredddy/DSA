04bea2e33262276f0fe40992a09b3f21fd1493dd
arunanaramredddy <159280945+arunanaramredddy@users.noreply.github.com>
2024-02-22 12:52:03 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+package Programs;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+	static void bubbleSort(int[] ar) {
+		for(int i=0;i<ar.length-1;i++) {
+			for(int j=0;j<ar.length-i-1;j++) {
+				if(ar[j]>ar[j+1]) {
+					int temp=ar[j];
+					ar[j]=ar[j+1];
+					ar[j+1]=temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] ar=new int[n];
+		for(int i=0;i<ar.length;i++) {
+			ar[i]=sc.nextInt();
+		}
+		bubbleSort(ar);
+		for(int i=0;i<ar.length;i++) {
+			System.out.print(ar[i]+ " ");
+		}
+		
+
+	}
+
+}
