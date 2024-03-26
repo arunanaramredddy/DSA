d27eb8f3ae00643cd66acc878699609861c1bcda
arunanaramredddy <159280945+arunanaramredddy@users.noreply.github.com>
2024-03-26 15:20:36 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,17 @@
+package com.work;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		System.out.println(5 | 9);
+		System.out.println(5 & 9);
+		System.out.println(5 ^ 9);
+		System.out.println(5 << 2);
+		System.out.println(5 >> 2);
+		System.out.println(-5 >> 1);
+		System.out.println(-5 >>> 1);
+
+		
+	}
+
+}
