c40fabe991e7d2948b4f6b09d304145021fef4c8
arunanaramredddy <159280945+arunanaramredddy@users.noreply.github.com>
2024-04-06 11:46:51 +0530

    Add files via upload

---
+++ PowerOf.java
@@ -0,0 +1,22 @@
+package com.work;
+
+public class PowerOf {
+	public static long pow(int x,int y) {
+		if(y==0) {
+			return 1;
+		}
+		if(y%2==0) {
+			long res=pow(x, y/2);
+			return res*res;
+		}
+		else {
+			return pow(x, y-1)*x;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(pow(5, 8));
+
+	}
+
+}
